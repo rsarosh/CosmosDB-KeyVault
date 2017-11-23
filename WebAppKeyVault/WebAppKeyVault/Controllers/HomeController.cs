811c157fd30d58424419f2ea088eab73e2d655e8
REDMOND\rafats <rafats@microsoft.com>
2017-11-22 17:10:42 -0800

    Initial push

---
+++ WebAppKeyVault/WebAppKeyVault/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace WebAppKeyVault.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            try
+            {
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var secret = await keyVaultClient.GetSecretAsync("https://rafatkeyvalut.vault.azure.net/secrets/testsecret/2c7b64d0c34546a5902095efcc44af10")
+                    .ConfigureAwait(false);
+
+                ViewBag.Secret = $"Secret: {secret.Value}";
+                
+            }
+            catch (Exception exp)
+            {
+                ViewBag.Error = $"Something went wrong: {exp.Message}";
+            }
+
+            ViewBag.Principal = azureServiceTokenProvider.PrincipalUsed != null ? $"Principal Used: {azureServiceTokenProvider.PrincipalUsed}" : string.Empty;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
